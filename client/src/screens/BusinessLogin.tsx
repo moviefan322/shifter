3a7e8779578f08589d52a1c1ed514155c8eec377
moviefan322 <philman202@yahoo.com>
2023-10-06 13:07:40 -0400

    adds Business Register/Login boilerplate

---
+++ client/src/screens/BusinessLogin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BusinessLogin = () => {
+  return (
+    <div>
+      <h1>Business Login</h1>
+    </div>
+  )
+}
+
+export default BusinessLogin
---
+++ client/src/screens/BusinessRegister.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BusinessRegister = () => {
+  return (
+    <div>
+      <h1>Business Register</h1>
+    </div>
+  )
+}
+
+export default BusinessRegister
